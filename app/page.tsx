65ab22fbb80fd120108f1db365dc69fa169701f8
leye <adeleyeifaturoti@yahoo.com>
2025-10-31 14:54:39 +0000

    added EventCard to page.tsx using dummp data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+
+const events = [
+    {title: "Event 1", image:"/images/event1.png"},
+    {title: "Event 2", image:"/images/event2.png"},
+]
 
 const Home = () => {
     return (
@@ -10,8 +16,10 @@ const Home = () => {
             <div className="mt-20 space-y-7">
                 <h3>Featured Events</h3>
                 <ul className={"events"}>
-                    {[1,2,3,4,5].map((event)=>(
-                        <li key={event}> Event {event}</li>
+                    {events.map((event)=>(
+                        <li key={event.title}>
+                            <EventCard {...event} />
+                        </li>
                     ))}
                 </ul>
             </div>
