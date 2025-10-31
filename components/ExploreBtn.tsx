670d48fbfca39687a52552b6e0a9f7eeac24e3b5
leye <adeleyeifaturoti@yahoo.com>
2025-10-31 13:59:55 +0000

    Explore Button Added

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Link from "next/link";
+import Image from "next/image";
+
+const ExploreBtn = () => {
+    return (
+        <button type="button" id="explore-btn" className="mt-7"  onClick={() => console.log("Clicked")}>
+            <Link href="#events">
+                Explore Events
+            </Link>
+            <Image src="/icons/arrow-down.svg" alt="arrow-down" width="24" height="24" />
+
+        </button>
+    )
+}
+export default ExploreBtn
